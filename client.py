5da8f64564da8a25cecbb0cefb79034ee2207206
riteshkhatri08 <riteshkhatri1512@gmail.com>
2025-09-29 18:30:40 +0530

    create a single connection client

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    message = input("Client: ")
+    while 'exit' not in message:
+        s.sendall(message.encode('utf-8'))
+        data = s.recv(1024)
+        print(f"Server {data!r}")
+        message = input("Client: ")
